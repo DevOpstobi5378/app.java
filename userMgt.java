a87db25d894463aff308c7c8913eb41a3d94d09a
DevOpstobi5378 <epeytheodore251@yahoo.com>
2023-03-01 13:57:28 +0000

    My second commit

--- userMgt.java
+++ userMgt.java
@@ -3,3 +3,12 @@ Oben
 Ayuk
 Epey
 This are my names
+<h2>Demo JavaScript in Head</h2>
+
+<p id="demo">Better Is Not Good Enough. The best is yet to come.</p>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+</body>
+</html> 
+
